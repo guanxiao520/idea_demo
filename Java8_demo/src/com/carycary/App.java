3dd41ab82786f83f7338fd3dce507b8f25f9ba57
cary <598539403@qq.com>
2021-05-15 14:42:03 +0800

    Initial commit

---
+++ Java8_demo/src/com/carycary/App.java
@@ -0,0 +1,13 @@
+package com.carycary;
+
+/**
+ * @author gx_hp
+ * @version V1.0
+ * @Package com.carycary
+ * @date 2021/5/15 13:45
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("java8");
+    }
+}
---
+++ idea2eclipse/src/App.java
@@ -0,0 +1,11 @@
+/**
+ * @author gx_hp
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2021/5/15 14:31
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("idea2eclipse");
+    }
+}
